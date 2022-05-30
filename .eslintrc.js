271caa8199bc343c08b3be02c706ffa0b70e3725
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-05-29 20:51:14 -0500

    Add task (13.1)  - Add README.md  - Install express, dotenv, pg, sequelize  - Install as devDependencies nodemon, prettier, eslint, eslint-plugin-prettier, eslit-config-prettier  - Add eslint config  - Add prettier config  - Ignore usual files with gitignore  - Add .env.example  - Create a Heroku application  - Add postgresql addon to Heroku app  - Test connection to postgresql database

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended', // / Make sure this is always the last element in the array.
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'no-console': 0,
+  },
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# Full Stack Open - Part 13 - [Using relational databases](https://fullstackopen.com/en/part13)
+
+Solutions for part 13 exercises
+
+## Commands
+
+Start by running `npm install` inside the project folder
+
+`npm start` to run the server  
+`npm run dev` to run the server in development mode
