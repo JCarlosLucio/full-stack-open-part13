a6f995e6a60265ed505eccc39a29de8319cc60ff
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-05-30 22:16:29 -0500

    Add task (13.6) - Add PUT /api/blogs/:id endpoint to update number of likes in a blog - Add blogFinder middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -18,12 +18,27 @@ router.post('/', async (req, res) => {
   }
 });
 
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id);
+  next();
+};
+
 /** DELETE /api/blogs/:id - Delete a blog */
-router.delete('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id);
-  if (blog) {
-    await blog.destroy();
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
     res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+/** PUT /api/blogs/:id - Update a blog number of likes*/
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes;
+    await req.blog.save();
+    res.json(req.blog);
   } else {
     res.status(404).end();
   }
