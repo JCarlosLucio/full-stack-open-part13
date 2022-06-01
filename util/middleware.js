bf43d591d1b8d1e773831c5a1bfcfeae0ae8cb07
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-01 11:20:33 -0500

    Add exercise (13.9) - Add validation for username to be email - Update errorHandler middleware to give more descriptive validation error messages - Update create_user request to use email in username field

--- util/middleware.js
+++ util/middleware.js
@@ -1,7 +1,9 @@
 const errorHandler = (error, req, res, next) => {
   console.log(error.message);
   if (error.name === 'SequelizeValidationError') {
-    return res.status(400).json({ error: error.message });
+    return res
+      .status(400)
+      .json({ error: error.errors.map((error) => error.message) });
   } else if (error.name === 'SequelizeDatabaseError') {
     return res.status(400).json({ error: error.message });
   }
