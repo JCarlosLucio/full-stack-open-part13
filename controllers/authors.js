927190d9376780f84b3c2b967212a03510065e81
Juan Carlos Lucio <c.lucio3@gmail.com>
2022-06-03 01:46:41 -0500

    Add task (13.16) - Add GET /api/authors endpoint to list all authors w/ number of articles and likes - Add get_authors request

---
+++ controllers/authors.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const { Blog } = require('../models');
+const { sequelize } = require('../util/db');
+
+/** GET /api/authors - List all authors with number of articles(blogs) and number of likes*/
+router.get('/', async (req, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      'author',
+      [sequelize.fn('COUNT', sequelize.col('*')), 'articles'],
+      [sequelize.fn('SUM', sequelize.col('likes')), 'likes'],
+    ],
+    group: ['author'],
+    order: [[sequelize.fn('SUM', sequelize.col('likes')), 'DESC']],
+  });
+
+  res.json(authors);
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('express-async-errors');
+const authorRouter = require('./controllers/authors');
 const blogRouter = require('./controllers/blogs');
 const loginRouter = require('./controllers/login');
 const userRouter = require('./controllers/users');
@@ -11,6 +12,7 @@ const middleware = require('./util/middleware');
 
 app.use(express.json());
 
+app.use('/api/authors', authorRouter);
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
